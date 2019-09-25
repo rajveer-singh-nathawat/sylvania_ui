533a8301ede286c6b202de693f78f33ccda4c252
rajveer <nathawatrajveer20@gmail.com>
2019-09-26 01:05:46 +0530

    side nav menu changes

--- src/app/home/sidebar/sidebar.component.ts
+++ src/app/home/sidebar/sidebar.component.ts
@@ -1,5 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+export interface MenuItem{
+  name?: string;
+  path?: string;
+  icon?: string;
+}
+const sideMenu :MenuItem[] = [{
+ name:'Dashboard',
+ path:'dashboard',
+ icon:'dashboard'
+},
+{
+  name:'Alummnus',
+  path:'alummnus',
+  icon:'people'
+},
+{
+  name:'Sylvanians',
+  path:'sylvanians',
+  icon:'people_outline'
+},
+{
+  name:'Gallery',
+  path:'gallery',
+  icon:'image'
+}];
 
+const secondryDefaultMenu : MenuItem[] = [{
+  name: 'Help',
+    icon: 'help_outline',
+    path: 'help'
+}];
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -7,9 +37,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  primaryMenu: MenuItem[] = [];
+  secondaryMenu: MenuItem[] = []
 
-  constructor() { }
+  constructor() {        
+     this.filterMenus(sideMenu, this.primaryMenu); 
+     this.filterMenus(secondryDefaultMenu, this.secondaryMenu);
 
+    }
+  private filterMenus(source, filtered) {
+
+    source.forEach(element => {
+      // const arg = [];
+      // arg.push(element.featureId);
+
+        filtered.push(element);
+
+    });
+
+  }
   ngOnInit() {
   }
 
