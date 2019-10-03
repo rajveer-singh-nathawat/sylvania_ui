55dd34462226aad0a16b614cbde05fbeb62a1a6b
rajveer.singh <rajveer.singh@meganexus.com>
2019-10-03 19:01:18 +0530

    register changes

--- src/app/sessions/register/register.component.ts
+++ src/app/sessions/register/register.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-export class RegisterData {
+export class RegistrationData {
   constructor(
-    fullName: string,
-    userName: string,
-    password: string,
-    confirmPassword: string,
-    email: string,
-    phoneNo: string,
-    status: string,
-    image: string
-  ){}
+    public  userName: string,
+    public  email: string,
+    public  password: string,
+    public  confirmPassword: string,
+    public  fullName: string,
+    public  phoneNo: string,
+    public  dateOfBirth: string,
+    public  qualification: string,
+    public  address: string,
+    public  livingStatus: boolean,
+    public  jobStatus: boolean,
+    public   designation: string,
+    public  jobLocation: string
+  ) { }
 }
+
 
 @Component({
   selector: 'app-register',
@@ -20,23 +26,39 @@ export class RegisterData {
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-isLinear = true;
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
-  thirdFormGroup: FormGroup;
+  registrationData: RegistrationData;
+  isLinear = true;
+  registrationFrom: FormGroup;
   constructor(private readonly builder: FormBuilder,
-              private readonly router: Router) { }
-
-              ngOnInit() {
-                this.firstFormGroup = this.builder.group({
-                  firstCtrl: ['', Validators.required]
-                });
-                this.secondFormGroup = this.builder.group({
-                  secondCtrl: ['', Validators.required]
-                });
-                this.thirdFormGroup = this.builder.group({
-                  thirdCtrl: ['', Validators.required]
-                });
-              }
-
+              private readonly router: Router) {}
+  ngOnInit() {
+      this.inItForm();
+      }
+  inItForm() {
+    this.registrationFrom = this.builder.group({
+      'accountDetailForm': this.builder.group({
+      userName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+  }),
+      'personalDetailForm': this.builder.group({
+      fullName: ['', Validators.required],
+      qualification: ['', Validators.required],
+      phoneNo: ['', Validators.required],
+      dOB: ['', Validators.required],
+      address: ['', Validators.required]
+  }),
+      'currentStatusForm':  this.builder.group({
+      livingStatus: ['', Validators.required],
+      jobStatus: ['', Validators.required],
+      designation: ['', Validators.required],
+      jobLocation: ['', Validators.required],
+  })
+});
+  }
+   submitRegistrationData() {
+    this.registrationData = this.registrationFrom.value;
+    console.log(this.registrationData);
+   }
 }
