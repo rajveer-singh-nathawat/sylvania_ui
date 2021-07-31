2ba48d3ed9d23c26d8dcaa52d62275747abe68f7
rajveer <nathawatrajveer20@gmail.com>
2021-07-31 22:06:10 +0530

    @angular/core migration - migration-v11-wait-for-async
    `async` to `waitForAsync` migration. The `async` testing function has been renamed to `waitForAsync` to avoid confusion with the native `async` keyword.

--- src/app/features/sylvanians/sylvanians.component.spec.ts
+++ src/app/features/sylvanians/sylvanians.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SylvaniansComponent } from './sylvanians.component';
 
@@ -6,7 +6,7 @@ describe('SylvaniansComponent', () => {
   let component: SylvaniansComponent;
   let fixture: ComponentFixture<SylvaniansComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SylvaniansComponent ]
     })
